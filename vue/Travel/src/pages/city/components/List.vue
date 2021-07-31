2247c3fe40d3809da1a69d6b839630b7f6888cfe
Tourscholar <krj1608178365@gmail.com>
2021-07-31 21:46:04 +0900

    fix bug

--- vue/Travel/src/pages/city/components/List.vue
+++ vue/Travel/src/pages/city/components/List.vue
@@ -1,32 +1,46 @@
 <template>
-    <div class="list" ref="wrapper">
-      <div>
-        <div class="area">
-          <div class="title border-topbottom">当前城市</div>
-          <div class="button-list">
-            <div class="button-wrapper">
-              <div class="button">北京</div>
-            </div>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
           </div>
         </div>
-        <div class="area">
-          <div class="title border-topbottom">热门城市</div>
-          <div class="button-list">
-            <div class="button-wrapper" v-for="item of hot" :key="item.id">
-              <div class="button">{{item.name}}</div>
-            </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div
+            class="button-wrapper"
+            v-for="item of hot"
+            :key="item.id"
+          >
+            <div class="button">{{item.name}}</div>
           </div>
         </div>
-        <div class="area" v-for="(item, key) of cities" :key="key">
-          <div class="title border-topbottom">{{key}}</div>
-          <div class="item-list">
-            <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
+      </div>
+      <div
+        class="area"
+        v-for="(item, key) of cities"
+        :key="key"
+        :ref="key"
+      >
+        <div class="title border-topbottom">{{key}}</div>
+        <div class="item-list">
+          <div
+            class="item border-bottom"
+            v-for="innerItem of item"
+            :key="innerItem.id"
+          >
+            {{innerItem.name}}
           </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
-
 <script>
 import BetterScroll from 'better-scroll'
 export default {
