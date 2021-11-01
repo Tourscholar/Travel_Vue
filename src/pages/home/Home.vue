83dc4deafa8224205a81c630822f07d588663ef4
Tourscholar <krj1608178365@gmail.com>
2021-11-01 13:47:08 +0900

    update vue3.0 composition API home page

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,69 +1,67 @@
 <template>
-    <div>
-      <home-header></home-header>
-      <home-swiper :list="swiperList"></home-swiper>
-      <home-icons :list="iconList"></home-icons>
-      <home-recommend :list="recommendList"></home-recommend>
-      <home-weekend :list="weekendList"></home-weekend>
-    </div>
+  <div>
+    <home-header></home-header>
+    <home-swiper :list="data.swiperList"></home-swiper>
+    <home-icons :list="data.iconList"></home-icons>
+    <home-recommend :list="data.recommendList"></home-recommend>
+    <home-weekend :list="data.weekendList"></home-weekend>
+  </div>
 </template>
 
 <script>
-import HomeHeader from './components/Header'
-import HomeSwiper from './components/Swiper'
-import HomeIcons from './components/Icons'
-import HomeRecommend from './components/Recommend'
-import HomeWeekend from './components/Weekend'
-import axios from 'axios'
-import { mapState } from 'vuex'
+// Composition API
+import HomeHeader from "./components/Header";
+import HomeSwiper from "./components/Swiper";
+import HomeIcons from "./components/Icons";
+import HomeRecommend from "./components/Recommend";
+import HomeWeekend from "./components/Weekend";
+import axios from "axios";
+import { useStore } from "vuex";
+import { reactive, onMounted } from "vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
     HomeRecommend,
-    HomeWeekend
+    HomeWeekend,
   },
-  data () {
-    return {
-      lastCity: '',
+  setup() {
+    const data = reactive({
+      // lastCity: "",
       swiperList: [],
       iconList: [],
       recommendList: [],
-      weekendList: []
-    }
-  },
-  methods: {
-    getHomeInfo () {
-      axios.get('/api/index.json?city=' + this.city)
-        .then(this.getHomeInfoSucc)
-    },
-    getHomeInfoSucc (res) {
-      res = res.data
+      weekendList: [], 
+    });
+    const store = useStore();
+    const city = store.state.city;
+    // 定义同步方法 
+    async function getHomeInfo() {
+      let res = await axios.get("/api/index.json?city=" + city);
+      res = res.data;
       if (res.ret && res.data) {
-        const data = res.data
-        this.swiperList = data.swiperList
-        this.iconList = data.iconList
-        this.recommendList = data.recommendList
-        this.weekendList = data.weekendList
+        const result = res.data;
+        data.swiperList = result.swiperList;
+        data.iconList = result.iconList;
+        data.recommendList = result.recommendList;
+        data.weekendList = result.weekendList;
       }
     }
+    onMounted(() => {
+      // data.lastCity = city;
+      getHomeInfo();
+    });
+    // onActivated(() => {
+    //   if (data.lastCity !== city) {
+    //     data.lastCity = city;
+    //     getHomeInfo();
+    //   }
+    // });
+    return { data };
   },
-  computed: {
-    ...mapState(['city'])
-  },
-  mounted () {
-    this.lastCity = this.city
-    this.getHomeInfo()
-  },
-  activated () {
-    if (this.lastCity !== this.city) {
-      this.lastCity = this.city
-      this.getHomeInfo()
-    }
-  }
-}
+};
 </script>
 
 <style>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,7 +2,7 @@
   <div class="wrapper">
     <swiper :options="swiperOptions" v-if="showSwiper">
       <swiper-slide v-for="item of list" :key="item.id">
-        <img  class="swiper-img" :src="item.imgUrl" />
+        <img class="swiper-img" :src="item.imgUrl" />
       </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
@@ -10,36 +10,37 @@
 </template>
 
 <script>
+import { computed } from "vue";
 export default {
-  name: 'HomeSwiper',
+  name: "HomeSwiper",
   props: {
-    list: Array
+    list: Array,
   },
-  data: function () {
-    return {
-      swiperOptions: {
-        pagination: '.swiper-pagination',
-        loop: true
-      }
-    }
+  setup(props) {
+    const swiperOptions = {
+      pagination: ".swiper-pagination",
+      loop: true,
+    };
+    const showSwiper = computed(() => props.list.length);
+    return { swiperOptions, showSwiper };
   },
-  computed: {
-    showSwiper () {
-      return this.list.length
-    }
-  }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
-  .wrapper >>> .swiper-pagination-bullet-active
-    background: #fff
-  .wrapper
-    overflow: hidden
-    width: 100%
-    height: 0
-    padding-bottom: 31.25%
-    background-color: #eee
-    .swiper-img
-      width: 100%
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+  background-color: #eee;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
 </style>
